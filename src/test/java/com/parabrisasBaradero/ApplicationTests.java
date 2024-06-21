ba7ed0bb0e595678035dae681d7be251d6a2559c
EliasPuddini1 <97805053+EliasPuddini@users.noreply.github.com>
2024-06-21 17:14:20 -0300

    Inicio

---
+++ src/test/java/com/parabrisasBaradero/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parabrisasBaradero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
